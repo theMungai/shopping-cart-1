e93241ffd8904f6d5d4993a8f13fdf1a10a9f77a
theMungai <mungaithesimon@gmail.com>
2025-01-17 11:16:54 +0300

    Cart logic

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let cart = []
 let productsHTML = ""
 products.forEach((product) => {
     productsHTML += `
@@ -13,7 +14,7 @@ products.forEach((product) => {
             </div>
 
             <div class="button-container">
-                <button class="add-to-cart">
+                <button class="add-to-cart js-add-to-cart-btn" data-product-name = "${product.name}" data-product-price = "${product.price.toFixed(2)}">
                     <img src="/assets/images/icon-add-to-cart.svg" alt="">
                     Add to cart
                 </button>
@@ -29,3 +30,75 @@ products.forEach((product) => {
 })
 
 document.querySelector(".products-grid").innerHTML = productsHTML
+
+
+// Looping through the buttons 
+const addToCartButtons = document.querySelectorAll(".js-add-to-cart-btn");
+addToCartButtons.forEach((addButton) => {
+    addButton.addEventListener("click", () => {
+        const productName = addButton.dataset.productName;
+        const productPrice = addButton.dataset.productPrice;
+
+        const existingProduct = cart.find(item => item.name === productName); 
+
+        if(existingProduct){
+            existingProduct.quantity += 1
+        }
+        else{
+            cart.push({
+                name : productName,
+                price : productPrice,
+                quantity : 1
+            })
+        }
+        updateCart()   
+    });
+});
+
+function updateCart(){
+    const cartContainer = document.querySelector(".cart-with-added-items");
+    const cartQuantity = document.querySelector(".js-cart-quantity");
+
+    let totalAmount = 0;
+
+    cart.forEach((item) => {
+        let cartHTML = `
+            <div class="cart-added-items">
+                <div class="added-items-details">
+                    <div class="added-item-name">
+                        <p class="full-name-product">${item.name}</p>
+                    </div>
+                    <div class="quantity-and-price">
+                        <div class="item-quantity">${item.quantity}x</div>
+                        <div class="price-per-item">@$${item.price}</div>
+                        <div class="product-of-price-and-quantity">$${(item.quantity * item.price).toFixed(2)}</div>
+                    </div>
+                </div>
+
+                <button class="remove-item-container">
+                    <img src="/assets/images/icon-remove-item.svg" alt="">
+                </button>
+            </div>
+            <div class="confirm-order">
+                    <button class="confirm-order-btn">Confirm Order</button>
+                </div>
+        `;
+
+        cartContainer.innerHTML += cartHTML
+        totalAmount += item.price * item.quantity;
+    })
+
+    
+    cartQuantity.textContent = cart.length
+
+    const totalHTML = `
+        <div class="total">
+            <p>Total</p>
+            <h1>$${totalAmount.toFixed(2)}</h1>
+        </div>
+        
+    `;
+    
+    cartContainer.innerHTML += totalHTML;
+
+}
